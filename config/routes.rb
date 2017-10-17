c5ae3401fd45cb8355bc5bf96da7eda26d389452
Daniel Cifuentes Mac <danielguarnizo@gmail.com>
2017-10-17 09:56:22 -0500

    modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 	resources :users
 	get 'users/:id/proccess', to: 'users#proccess'
 	match "/delayed_job" => DelayedJobWeb, :anchor => false, :via => [:get, :post]
-  resources :people
-  resources :users
+	resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
